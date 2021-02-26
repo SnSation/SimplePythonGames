b3aeb39a07e798dcd6573f5813747b742b21304f
SnSation <blair95273@gmail.com>
2021-02-26 02:45:13 -0600

    added different import statements to main for a demo

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
 from blackjack.game import Game
+print(1)
+blackjack_1 = Game()
+blackjack_1.start()
 
-blackjack_game = Game()
-blackjack_game.start()
+from blackjack import Blackjack
+print(2)
+blackjack_2 = Blackjack.new()
+blackjack_2.start()
+
+import blackjack
+print(3)
+blackjack_3 = blackjack.game.Game()
+blackjack_3.start()
+
+print(4)
+blackjack_4 = blackjack.Blackjack.new()
+blackjack_4.start()
